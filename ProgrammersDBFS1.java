996dd94c1e1c38869639c92a39660171f8fefa36
younditt <dhy039@gmail.com>
2020-04-28 17:58:32 +0900

    algorithm study

---
+++ ProgrammersDBFS1.java
@@ -0,0 +1,19 @@
+public class ProgrammersDBFS1{
+    public static void main(String[] args) {
+        System.out.println("Default Main Fuction Sample");
+
+    }
+
+    static int solution(int[] numbers, int target) {
+        int answer = 0;
+        //x*1, x*-1
+        int result = 0 ; 
+        int x = 1;
+        //result+=x*1
+    }
+
+    int dfs(){
+
+    }
+
+}
--- ProgrammersSearch4.java
+++ ProgrammersSearch4.java
@@ -11,6 +11,26 @@ public class ProgrammersSearch4 {
         //result[0]-2 X result[1]-2 == red의 가로세로
         //red가로 *2 = red세로*2 + 4 = brown 개수
 
+        
+
+        //면적 : red+brown = column*row
+        //둘레 : brown = column+row
+        //red = (row-2)*((brown - row)-2)
+
+        //column = brown - row;
+        //red + brown = (brown-row)*row
+        //row^2 - row*brown + brown - (row-2)*( (brown - row) -2) = 0;
+        //row^2 - row*brown + brown +2row-4+(row-2)*brown - (row-2)*row = 0
+                                        // +row*brown - 2brown - row^2 + 2row
+        //brown + row*brown + 2row = 0;
+        // 2row + row*brown = -brown;
+        //row(2+brown) = -brown;
+        // row = -brown/(brown+2);
+
+
+
+
+
         //가로가 같거나 더 길다.
         //row*2 + column*2 = brown+4;
             //row = ((brown+4)-2column)/2
