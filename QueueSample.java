19e973d2701ed4c90f122e3c5c4612bc4b5e3e98
younditt <dhy039@gmail.com>
2020-05-14 19:59:16 +0900

    study repository 정리

---
+++ QueueSample.java
@@ -0,0 +1,31 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 큐 클래스로 구현된 스택과 달리 별도의 인터페이스 형태로 제공됨 
+ * 1. Deque : LinkedList로 구현되었으며, 가장 자주 사용됨.
+ * 2. BlockingDeque 3. BlockingQueue 4. TransferQueue 5. LinkedList를 형변환하여 객체생성
+ * 가능
+ * 
+ * add remove element offer poll peek
+ * 
+ */
+public class QueueSample{
+    public static void main(String[] args) {
+        System.out.println("Default Main Fuction Sample");
+
+        //PriorityQueue
+        //LinkedList
+        //Deque
+        Queue<String> queue = new LinkedList<String>();
+		for(int i=0;i<3;i++)
+			queue.add("데이터-"+i); //큐에 데이터를 삽입한다.
+		
+		//queue = ["데이터-0", "데이터-1", "데이터-2"], front=0, rear=2
+		
+		System.out.println(queue.peek()); //큐의 front가 가리키는 값을 반환한다.         데이터-0
+		System.out.println(queue.poll()); //큐의 front가 가리키는 값을 반환하고 삭제한다.    데이터-0
+		System.out.println(queue.isEmpty()); // false
+		System.out.println(queue);			//[데이터-1, 데이터-2]
+    }
+}
---
+++ SymmetricTree101.java
@@ -0,0 +1,15 @@
+  class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+public class SymmetricTree101{
+    public static void main(String[] args) {
+        System.out.println("Default Main Fuction Sample");
+    }
+    
+    static boolean isSymmetric(TreeNode root) {
+    }
+}
