92d0a4d075176980217e8c9be21f213b979f83f3
younditt <dhy039@gmail.com>
2020-10-20 17:03:29 +0900

    ohms/week22

--- src/main/java/algorithm/structure/Programmers42579.java
+++ src/main/java/algorithm/structure/Programmers42579.java
@@ -1,6 +1,5 @@
 package algorithm.structure;
 
-import org.jetbrains.annotations.NotNull;
 
 import java.util.*;
 
@@ -15,68 +14,47 @@ import java.util.*;
  */
 //푸는중
 public class Programmers42579 {
-    class Album {
+    class Album implements Comparable<Album> {
         int code;
         int plays;
-        int total;
-        String genre;
 
         Album(int code, int plays, String genre) {
             this.code = code;
             this.plays = plays;
-            this.genre = genre;
-            this.total += plays;
         }
 
-        void setTotal(int total) {
-            this.total = total + this.plays;
+        @Override
+        public int compareTo(Album o) {
+            return this.plays - o.plays;
         }
-
     }
 
     public int[] solution(String[] genres, int[] plays) {
         List<Integer> answer = new ArrayList<>();
 
-        Map<String, List<Album>> bestAlbum = new HashMap<>();
+        Map<String, Integer> playTime = new HashMap<>();
+        Map<String, PriorityQueue<Album>> bestAlbum = new HashMap<>();
 
         for (int i = 0; i < genres.length; i++) {
             Album newAlbum = new Album(i, plays[i], genres[i]);
-            List<Album> list;
-            if (bestAlbum.containsKey(genres[i])) {
-                list = bestAlbum.get(genres[i]);
-                newAlbum.setTotal(list.get(list.size() - 1).total);
-            } else {
-                list = new ArrayList<>();
-            }
-//            PriorityQueue<Album> list = bestAlbum.getOrDefault(genres[i], new PriorityQueue<>());
+            PriorityQueue<Album> list = bestAlbum.getOrDefault(genres[i], new PriorityQueue<>(Collections.reverseOrder()));
             list.add(newAlbum);
             bestAlbum.put(genres[i], list);
+            playTime.put(genres[i], playTime.getOrDefault(genres[i], 0) + plays[i]);
         }
-//        PriorityQueue<Album> keys = new PriorityQueue<>(bestAlbum.size(), Collections.reverseOrder());
-//        for (List<Album> albums : bestAlbum.values()) {
-//            Album album = albums.get(albums.size() - 1);
-//            keys.add(album);
-////            keys.put(album.genre, album.total);
-//        }
-        Album[] keys = new Album[bestAlbum.size()];
-//        Arrays.sort(keys, ((o1, o2) -> {
-//            return Integer.compare(o2.total, o1.total);
-//        }));
+        //playTime을 정렬하고, 그 순서대로 bestAlbum에서 꺼내서 출력하기.
+        playTime.entrySet().stream()
+                .sorted((o1, o2) -> o2.getValue() - o1.getValue())
+                .forEach(entry -> {
+                    PriorityQueue<Album> list = bestAlbum.get(entry.getKey());
+                    if (list.size() >= 2) {
+                        answer.add(list.poll().code);
+                        answer.add(list.poll().code);
+                    } else {
+                        answer.add(list.poll().code);
+                    }
+                });
 
-        Arrays.sort(keys, ((o1, o2) -> Integer.compare(o1.total, o2.total)));
-
-
-
-        for (Album key : keys) {
-            int i = 2; // maximum
-            List<Album> albums = bestAlbum.get(key.genre);
-            while (albums.size() != 0 && i != 0) {
-                Album a = albums.get(albums.size() - 1);
-                answer.add(a.code);
-                albums.remove(a);
-                i--;
-            }
-        }
         return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
