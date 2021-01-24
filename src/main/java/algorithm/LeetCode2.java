a1f7f4cb269b02e8109e4b4e85757f631f65dba2
YounDit <dhy039@gmail.com>
2021-01-24 10:50:25 +0900

    Update LeetCode2.java
    add kotlin solution

--- src/main/java/algorithm/LeetCode2.java
+++ src/main/java/algorithm/LeetCode2.java
@@ -1,4 +1,33 @@
 class LeetCode2{
+    //Kotlin
+    /*
+    fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
+        var additionalDigit = 0
+        var head: ListNode? = null
+
+        var node1 = l1
+        var node2 = l2
+        var result = head
+        while(node1 != null || node2 != null) {
+            val sum = additionalDigit + (node1?.`val`?:0) + (node2?.`val`?:0)
+            val sumListNode = ListNode(sum % 10)
+            if (result != null) {
+                result.next = sumListNode
+                result = result.next
+            } else {
+                result = sumListNode
+                head = result
+            }
+            additionalDigit = sum / 10
+            node1 = node1?.next
+            node2 = node2?.next
+        }
+        if(additionalDigit != 0){
+            result?.next = ListNode(additionalDigit)
+        }
+        return head
+    }
+    */
     public static  ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
         ListNode head = null;
