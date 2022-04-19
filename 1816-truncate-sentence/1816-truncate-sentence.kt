2fa0b913a9e8928b90e236125c29716ea6166511
YounDit <dhy039@gmail.com>
2022-04-19 19:15:44 +0900

    Time: 218 ms (75.00%), Space: 37 MB (28.57%) - LeetHub

--- 1816-truncate-sentence/1816-truncate-sentence.kt
+++ 1816-truncate-sentence/1816-truncate-sentence.kt
@@ -1,20 +1,21 @@
 class Solution {
     fun truncateSentence(s: String, k: Int): String {
-  var result = ""
-        var input= s
-        var count = k
-        while (input.isNotBlank() && count != 0) {
-            val index = input.indexOf(" ", 0)
-            if (index == -1) {
-                result += input
-                break
-            } else {
-                count--
-                result += input.substring(0, index+1)
-                input = input.removeRange(0, index+1)
-//                input = input.substring(index+1, input.length)
-            }
-        }
-        return result.trim()
+       return s.split(" ").take(k).joinToString(" ").trim();
+//   var result = ""
+//         var input= s
+//         var count = k
+//         while (input.isNotBlank() && count != 0) {
+//             val index = input.indexOf(" ", 0)
+//             if (index == -1) {
+//                 result += input
+//                 break
+//             } else {
+//                 count--
+//                 result += input.substring(0, index+1)
+//                 input = input.removeRange(0, index+1)
+// //                input = input.substring(index+1, input.length)
+//             }
+//         }
+//         return result.trim()
     }
 }
