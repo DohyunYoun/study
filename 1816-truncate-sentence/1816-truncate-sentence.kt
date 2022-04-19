be798c396266b36b3862febc96e7b5279a1274ab
YounDit <dhy039@gmail.com>
2022-04-19 19:14:45 +0900

    Time: 176 ms (92.86%), Space: 36.5 MB (50.00%) - LeetHub

---
+++ 1816-truncate-sentence/1816-truncate-sentence.kt
@@ -0,0 +1,20 @@
+class Solution {
+    fun truncateSentence(s: String, k: Int): String {
+  var result = ""
+        var input= s
+        var count = k
+        while (input.isNotBlank() && count != 0) {
+            val index = input.indexOf(" ", 0)
+            if (index == -1) {
+                result += input
+                break
+            } else {
+                count--
+                result += input.substring(0, index+1)
+                input = input.removeRange(0, index+1)
+//                input = input.substring(index+1, input.length)
+            }
+        }
+        return result.trim()
+    }
+}
