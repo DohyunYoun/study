cb6b929591e4963c0046f256b3e3f12fcb98f553
YounDit <dhy039@gmail.com>
2022-05-22 20:22:17 +0900

    Time: 226 ms (94.12%), Space: 37.9 MB (88.24%) - LeetHub

--- 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.kt
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.kt
@@ -1,12 +1,14 @@
 class Solution {
     fun minOperations(nums: IntArray): Int {
-        if(nums.size <2) return 0
+            var left = -1
         var result = 0
-        (1..nums.size-1).forEach { i ->
-           if (nums[i - 1] >= nums[i]){
-            result += nums[i - 1] - nums[i] + 1
-            nums[i] += nums[i - 1] - nums[i] + 1
-           }
+        nums.forEach { right->
+            if (left >= right) {
+                result += left - right + 1
+                left++
+            } else {
+                left = right
+            }
         }
         return result
     }
