5fbea436856cde421fc94c0b30c53017d6d92008
YounDit <dhy039@gmail.com>
2023-06-03 13:39:21 +0900

    Time: 2352 ms (33.33%), Space: 60.3 MB (100.00%) - LeetHub

---
+++ 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.kt
@@ -0,0 +1,27 @@
+class Solution {
+         fun maximumSum(nums: IntArray): Int {
+ val sumOfDigits = nums.map { num -> num.toString().map { it.toInt() - '0'.toInt() }.sum() }
+        var maxSum = -1
+        val trash = arrayListOf<Int>()
+
+        sumOfDigits.forEachIndexed { index, num ->
+            if (!trash.contains(num)) {
+                val pairIndex = arrayListOf<Int>()
+                sumOfDigits.forEachIndexed { index, i ->
+                    if (i == num)
+                        pairIndex.add(index)
+                }
+
+                if (pairIndex.size > 1) {
+                    trash.add(num)
+                    for (i in 0 until pairIndex.size) {
+                        for (j in i + 1 until pairIndex.size) {
+                            maxSum = Math.max(maxSum, nums[pairIndex[i]] + nums[pairIndex[j]])
+                        }
+                    }
+                }
+            }
+        }
+        return maxSum
+         }
+}
